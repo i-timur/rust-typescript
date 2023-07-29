7ff9656961b206bca625588eaf3a39825d88cf05
i-timur <i.timur@mail.ru>
2023-07-29 17:58:35 +0300

    feat: rust-typescript

---
+++ index.ts
@@ -0,0 +1,4 @@
+function practice(value: number | undefined): undefined | number {
+    return value === undefined ? undefined : value * 5;
+}
+
---
+++ src/shapes/area.rs
@@ -0,0 +1,11 @@
+use super::rect::Rect;
+
+pub trait Area {
+    fn area(&self) -> f64;
+}
+
+impl Area for f64 {
+    fn area(&self) -> f64 {
+        return self * self;
+    }
+}
